395b64628e1f78a8027b82335f50b76cf7670e4d
Jeffrey Lee <email": null>
2016-10-30 17:11:17 -0700

    update category show paths

--- cl-clone/app/controllers/articles_controller.rb
+++ cl-clone/app/controllers/articles_controller.rb
@@ -1,2 +1,24 @@
 class ArticlesController < ApplicationController
+  before_action :set_article
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
--- cl-clone/app/controllers/categories_controller.rb
+++ cl-clone/app/controllers/categories_controller.rb
@@ -1,2 +1,19 @@
 class CategoriesController < ApplicationController
+
+  before_action :set_category
+
+  def index
+    @categories = Category.all.order(:name)
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
 end
